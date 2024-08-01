0a857c38a2696f60f23d6851dc0cacef121c3db5
meddev <mhamedalyacoubi@gmail.com>
2024-08-01 08:36:25 +0100

    update

---
+++ src/components/transalteiceberti/Translateiceberti.jsx
@@ -0,0 +1,12 @@
+import './transalteiceberti.css';
+
+export default function Translateiceberti() {
+    return (
+        <div className='translateiceberti'>
+            <div className="content">
+                <p>IceBerti Mirleft <span>Is The Best</span> IceBerti Mirleft <span>Is The Best</span> IceBerti Mirleft <span>Is The Best</span> IceBerti Mirleft <span>Is The Best</span> IceBerti Mirleft <span>Is The Best </span> </p>
+                <p>IceBerti Mirleft <span>Is The Best</span> IceBerti Mirleft <span>Is The Best</span> IceBerti Mirleft <span>Is The Best</span> IceBerti Mirleft <span>Is The Best</span> IceBerti Mirleft <span>Is The Best </span> </p>
+            </div>
+        </div>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Menu from '../components/menu/Menu'
 import Nav from '../components/navbar/Nav'
 import Story from '../components/story/Story'
 import Swipermenu from '../components/swipermenu/Swipermenu'
+import Translateiceberti from '../components/transalteiceberti/Translateiceberti'
 
 export default function Home() {
     return (
@@ -9,6 +10,7 @@ export default function Home() {
             <Nav />
             <Story />
             <Swipermenu />
+            <Translateiceberti />
             <Menu />
         </>
     )
