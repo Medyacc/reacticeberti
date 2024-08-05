065b6a84bbf0c50942bda88f26c8425cc22f59c7
meddev <mhamedalyacoubi@gmail.com>
2024-08-05 18:47:57 +0100

    update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,38 @@
 import './footer.css'
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import RoomIcon from '@mui/icons-material/Room';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Footer() {
     return (
         <div className='footer'>
             <div className='container'>
-                Footer
+                <div className="footerleft">
+                    <div className="footersocial">
+                        <img src="./images/logo.png" alt="" />
+                        <div className='socialicons'>
+                            <FacebookIcon />
+                            <InstagramIcon />
+                        </div>
+                    </div>
+                    <div className="footerplace">
+                        <p><RoomIcon className='footerplaceIcon' /> AV MOHAMED 5 EL YOUSSOFIA Tiznit</p>
+                        <p><MailOutlineIcon className='footerplaceIcon' /> info@icebertimirlet.com</p>
+                    </div>
+                </div>
+                <div className="footerright">
+                    <div className='footerprivacy'>
+                        <a><KeyboardArrowRightIcon /><span>Privacy Policy</span></a>
+                        <a><KeyboardArrowRightIcon /><span>Cookie Policy</span></a>
+                        <a><KeyboardArrowRightIcon /><span>Terms and Conditions</span></a>
+                    </div>
+                    <div className='footerfaq'>
+                        <a>FAQs</a>
+                        <a>Contact Us</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
