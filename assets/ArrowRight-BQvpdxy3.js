e444ce11ef6829a2fb31643264ba36148b3886bd
Medyacc <Medyacc@users.noreply.github.com>
2024-08-10 16:23:31 +0000

    deploy: 4028a08a86251a687b237f97711e06efd3145201

---
+++ assets/ArrowRight-BQvpdxy3.js
@@ -0,0 +1,1 @@
+import{r,i as t}from"./createSvgIcon-BIY7pBIO.js";import{j as a}from"./index-CjuK0BFJ.js";var e={},o=t;Object.defineProperty(e,"__esModule",{value:!0});var i=e.default=void 0,u=o(r()),s=a;i=e.default=(0,u.default)((0,s.jsx)("path",{d:"m10 17 5-5-5-5z"}),"ArrowRight");export{i as d};
