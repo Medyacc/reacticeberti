b68b6f48e6142b2840006d58ea294a00cb25a473
Medyacc <Medyacc@users.noreply.github.com>
2024-08-11 15:59:20 +0000

    deploy: fd1422ddc1db15246d2fcf5160cce7740474cc6b

---
+++ assets/Ouriceddrinks-C5JhRsAl.js
@@ -0,0 +1,1 @@
+import{u as s,j as r}from"./index-CbO4aOkU.js";function a(){return s(),r.jsx("div",{className:"ouriceddrinks"})}export{a as default};
--- assets/Ouriceddrinks-DZpdrieo.js
+++
@@ -1,1 +0,0 @@
-import{u as s,j as r}from"./index-B_3Tdhh4.js";function a(){return s(),r.jsx("div",{className:"ouriceddrinks"})}export{a as default};
