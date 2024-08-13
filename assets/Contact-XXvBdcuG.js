b6711f0cc434b12b896c45a5155a278168f99748
Medyacc <Medyacc@users.noreply.github.com>
2024-08-13 23:55:00 +0000

    deploy: d9ab3d1937e52ce241dd40d0ef7ed93b1aca3442

---
+++ assets/Contact-XXvBdcuG.js
@@ -0,0 +1,1 @@
+import{u as t,j as a}from"./index-CVb5iUhx.js";function n(){return t(),a.jsx("div",{className:"contact"})}export{n as default};
--- assets/Ouriceddrinks-CQp0dr-q.js
+++
@@ -1,1 +0,0 @@
-import{u as s,j as r}from"./index-BTATtuXo.js";function a(){return s(),r.jsx("div",{className:"ouriceddrinks"})}export{a as default};
