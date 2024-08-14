2e368d5951dd1bd0ea33262a33a34a9f8b796312
meddev <mhamedalyacoubi@gmail.com>
2024-08-14 01:37:33 +0100

    update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import './contact.css'
+import PhoneIcon from '@mui/icons-material/Phone';
 
 export default function Contact() {
 
@@ -7,9 +8,29 @@ export default function Contact() {
 
     return (
         <div className='contact'>
-            <div className="address"></div>
-            <div className="contact"></div>
-            <div className="hours"></div>
+            <div className='container'>
+                <div className="address">
+                    <h3>Address</h3>
+                    <p>Showgate Center</p>
+                    <p>Corner Curie Avenue & Passteur Drive</p>
+                    <p>Bloemfontein 9300</p>
+                    <a>Get Directions</a>
+                </div>
+                <div className="numphone">
+                    <h3>Contact</h3>
+                    <p><PhoneIcon className='phoneIcon' /> 051 447 8046</p>
+                </div>
+                <div className="hours">
+                    <h3>Hours</h3>
+                    <p><span>Sun</span> Open 24 hours</p>
+                    <p><span>Mun</span> Open 24 hours</p>
+                    <p><span>Tue</span> Open 24 hours</p>
+                    <p><span>Wed</span> Open 24 hours</p>
+                    <p><span>Thu</span> Open 24 hours</p>
+                    <p><span>Fri</span> Open 24 hours</p>
+                    <p><span>Sat</span> Open 24 hours</p>
+                </div>
+            </div>
         </div>
     )
 }
