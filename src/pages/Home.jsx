754656da11dbbb7646108f15b31cc6ef26abc662
meddev <mhamedalyacoubi@gmail.com>
2024-08-14 01:49:00 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import './home.css'
 import React, { Suspense } from 'react';
-// const Footer = React.lazy(() => import('../components/footer/Footer'));
+const Footer = React.lazy(() => import('../components/footer/Footer'));
 const Nav = React.lazy(() => import('../components/navbar/Nav'));
 const Contact = React.lazy(() => import('../components/contact/Contact'));
 const PureWater = React.lazy(() => import('../components/purewater/PureWater'));
@@ -8,24 +8,21 @@ const Scrolltotop = React.lazy(() => import('../components/scrolltotop/Scrolltot
 const Story = React.lazy(() => import('../components/story/Story'));
 const Swipermenu = React.lazy(() => import('../components/swipermenu/Swipermenu'));
 const Translateiceberti = React.lazy(() => import('../components/transalteiceberti/Translateiceberti'));
-import Footer from '../components/footer/Footer'
 
 export default function Home() {
     return (
-        <>
-            <Suspense fallback={<div className='loading'>
-                <div className="ring"></div>
-                <span>loading...</span>
-            </div>}>
-                <Scrolltotop />
-                <Nav />
-                <Story />
-                <Swipermenu />
-                <PureWater />
-                <Translateiceberti />
-                <Contact />
-            </Suspense>
+        <Suspense fallback={<div className='loading'>
+            <div className="ring"></div>
+            <span>loading...</span>
+        </div>}>
+            <Scrolltotop />
+            <Nav />
+            <Story />
+            <Swipermenu />
+            <PureWater />
+            <Translateiceberti />
+            <Contact />
             <Footer />
-        </>
+        </Suspense>
     );
 }
