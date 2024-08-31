0e226f3f7547c6616c43f9f37cce51c4a2e99892
Medyacc <Medyacc@users.noreply.github.com>
2024-08-31 20:28:32 +0000

    deploy: f378ceb303e8cd1c4f52f64c5cee0b9996e67610

--- assets/Truncatetext-DfAspvNy.js
+++
@@ -1,1 +0,0 @@
-import{a as c,u,j as r}from"./index-vl9lfWER.js";function p({text:s,maxLength:e}){const[t,n]=c.useState(!1),o=s.length>e?s.substring(0,e)+"...":s,{t:a}=u();return r.jsxs("p",{className:"truncatedtext",children:[t?s:o,s.length>e&&r.jsx("span",{className:"toggle-text",onClick:()=>n(!t),children:a(t?"purewater.showless":"purewater.readmore")})]})}export{p as T};
