5b06d80d545b16baeb75beef60e3f0cf8d554a85
Medyacc <Medyacc@users.noreply.github.com>
2024-08-31 23:08:53 +0000

    deploy: 170efa2e2197a1870173192995790ec6677ac7e9

---
+++ assets/Truncatetext-CbibciMn.js
@@ -0,0 +1,1 @@
+import{a as c,u,j as r}from"./index-DP9zBR6r.js";function p({text:s,maxLength:e}){const[t,n]=c.useState(!1),o=s.length>e?s.substring(0,e)+"...":s,{t:a}=u();return r.jsxs("p",{className:"truncatedtext",children:[t?s:o,s.length>e&&r.jsx("span",{className:"toggle-text",onClick:()=>n(!t),children:a(t?"purewater.showless":"purewater.readmore")})]})}export{p as T};
