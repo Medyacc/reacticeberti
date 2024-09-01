12b228eeefec2da2d99a2aea6c1d36b0ada42667
meddev <mhamedalyacoubi@gmail.com>
2024-09-01 15:29:14 +0100

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,21 @@
 import './home.css'
-import Scrolltotop from '../../components/scrolltotop/Scrolltotop'
-import Nav from '../../components/navbar/Nav'
-import Story from '../../components/story/Story'
-import Swipermenu from '../../components/swipermenu/Swipermenu'
-import PureWater from '../../components/purewater/PureWater'
-import Translateiceberti from '../../components/transalteiceberti/Translateiceberti'
-import Placeimages from '../../components/placeImages/Placeimages'
-import Contact from '../../components/contact/Contact'
-import Footer from '../../components/footer/Footer'
-// const Footer = React.lazy(() => import('../../components/footer/Footer'));
-// const Nav = React.lazy(() => import('../../components/navbar/Nav'));
-// const Contact = React.lazy(() => import('../../components/contact/Contact'));
-// const PureWater = React.lazy(() => import('../../components/purewater/PureWater'));
-// const Scrolltotop = React.lazy(() => import('../../components/scrolltotop/Scrolltotop'));
-// const Story = React.lazy(() => import('../../components/story/Story'));
-// const Swipermenu = React.lazy(() => import('../../components/swipermenu/Swipermenu'));
-// const Translateiceberti = React.lazy(() => import('../../components/transalteiceberti/Translateiceberti'));
-// const Placeimages = React.lazy(() => import('../../components/placeImages/Placeimages'));
+import React, { Suspense } from 'react';
+const Footer = React.lazy(() => import('../../components/footer/Footer'));
+const Nav = React.lazy(() => import('../../components/navbar/Nav'));
+const Contact = React.lazy(() => import('../../components/contact/Contact'));
+const PureWater = React.lazy(() => import('../../components/purewater/PureWater'));
+const Scrolltotop = React.lazy(() => import('../../components/scrolltotop/Scrolltotop'));
+const Story = React.lazy(() => import('../../components/story/Story'));
+const Swipermenu = React.lazy(() => import('../../components/swipermenu/Swipermenu'));
+const Translateiceberti = React.lazy(() => import('../../components/transalteiceberti/Translateiceberti'));
+const Placeimages = React.lazy(() => import('../../components/placeImages/Placeimages'));
 
 export default function Home() {
     return (
-        <>
+        <Suspense fallback={<div className='loading'>
+            <div className="ring"></div>
+            <span>loading...</span>
+        </div>}>
             <Scrolltotop />
             <Nav />
             <Story />
@@ -30,6 +25,6 @@ export default function Home() {
             <Placeimages />
             <Contact />
             <Footer />
-        </>
+        </Suspense>
     );
 }
